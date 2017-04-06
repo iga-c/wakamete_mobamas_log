dc5b58754003788cd829868055e22ba3dc8789c1
Kazuya Igari <kazuya.igari@beaglescience.com>
2017-04-06 20:49:10 +0900

    :sparkles: village_typeメソッドで探偵村を判断できるように修正

--- mobamas_parser.rb
+++ mobamas_parser.rb
@@ -1,5 +1,14 @@
 require_relative 'wakamete_parser'
 
 class MobamasParser < WakameteParser
+  def village_type
+    type = super
+    tr_array = first_night_tr
 
+    if tr_array.any?{|tr| tr.text.start_with?('◆ゲームマスター') && tr.text.include?('※もし探偵を噛んだ場合は') }
+      type = '探偵'
+    end
+
+    type
+  end
 end
