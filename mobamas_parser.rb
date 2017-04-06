5da07e6ef6902b2a747cfe5b255fbe53cebd87e3
Kazuya Igari <kazuya.igari@beaglescience.com>
2017-04-06 21:14:57 +0900

    :sparkles: roleメソッドで狂信者と背徳者を判定出来るように修正

--- mobamas_parser.rb
+++ mobamas_parser.rb
@@ -11,4 +11,22 @@ class MobamasParser < WakameteParser
 
     type
   end
+
+  def role
+    result_hash = super
+    tr_array = first_night_tr
+
+    if tr_array.any?{|tr| tr.text.start_with?('◆ゲームマスター') && tr.text.include?('狂人の人は乱数表を')}
+      result_hash['狂人'] = 0
+      result_hash['狂信者'] = 1
+    end
+
+    tr_array = first_night_tr
+    if tr_array.any?{|tr| tr.text.start_with?('◆ゲームマスター') && tr.text.include?('被った場合は背徳者になれません')}
+      result_hash['村人'] -= 1
+      result_hash['背徳者'] = 1
+    end
+
+    result_hash
+  end
 end
